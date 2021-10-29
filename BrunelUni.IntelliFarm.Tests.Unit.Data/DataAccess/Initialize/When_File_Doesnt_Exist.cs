8ad013b0304872ebd7f5cc582300f5258468c3e5
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-29 02:06:44 +0100

    added tests for when file doesnt exist ( blender animation context ) - added failing implementation - tests fail

---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/Initialize/When_File_Doesnt_Exist.cs
@@ -0,0 +1,34 @@
+﻿using Aidan.Common.Core;
+using Aidan.Common.Core.Enum;
+using BrunelUni.IntelliFarm.Data.Core.Dtos;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.Initialize
+{
+    public class When_File_Doesnt_Exist : Given_A_BlenderAnimationContext
+    {
+        private Result _result;
+
+        protected override void When( )
+        {
+            MockFileAdapter.Exists( Arg.Any<string>( ) )
+                .Returns( Result.Error( "" ) );
+            _result = SUT.Initialize( "" );
+        }
+
+        [ Test ]
+        public void Then_Failiure_Occurs( )
+        {
+            Assert.AreEqual( _result.Status, OperationResultEnum.Failed );
+        }
+
+        [ Test ]
+        public void Then_Render_Manager_Is_Not_Created( )
+        {
+            MockRenderManagerFactory
+                .DidNotReceive( )
+                .Factory( Arg.Any<RenderMetaDto>( ) );
+        }
+    }
+}
