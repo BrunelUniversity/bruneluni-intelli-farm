8b69dc162eeb0a58b724abe25d36483ce4069b64
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-29 16:39:09 +0100

    added tests for temporary data file to be read invalid - added failing implementation - tests fail

---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/SceneProcessorTests/ReadTemp/When_File_Error_Occurs.cs
@@ -0,0 +1,28 @@
+﻿using Aidan.Common.Core;
+using Aidan.Common.Core.Enum;
+using BrunelUni.IntelliFarm.Data.Core.Dtos;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.SceneProcessorTests.ReadTemp
+{
+    public class When_File_Error_Occurs : Given_A_SceneProcessor
+    {
+        private ObjectResult<RenderDataDto> _result;
+        private const string Message = "bubble up";
+
+        protected override void When( )
+        {
+            MockFileAdapter.ReadFile( Arg.Any<string>( ) ).Returns( new ObjectResult<string>
+                { Status = OperationResultEnum.Failed, Msg = Message } );
+            _result = SUT.ReadTemp<RenderDataDto>( );
+        }
+
+        [ Test ]
+        public void Then_Failiure_Is_Returned( )
+        {
+            Assert.AreEqual( OperationResultEnum.Failed, _result.Status );
+            Assert.AreEqual( Message, _result.Msg );
+        }
+    }
+}
