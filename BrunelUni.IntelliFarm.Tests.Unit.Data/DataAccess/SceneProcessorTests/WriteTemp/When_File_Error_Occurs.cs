a3f6fcd3db9d1367b66ea758b3c4e9e722ff71ba
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-30 21:17:14 +0100

    add tests for writing to temp file blender - added failing implementation for file system failure - tests fail

---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/SceneProcessorTests/WriteTemp/When_File_Error_Occurs.cs
@@ -0,0 +1,28 @@
+﻿using Aidan.Common.Core;
+using Aidan.Common.Core.Enum;
+using BrunelUni.IntelliFarm.Data.Core.Dtos;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.SceneProcessorTests.WriteTemp
+{
+    public class When_File_Error_Occurs : Given_A_SceneProcessor
+    {
+        private Result _result;
+        private const string MessageToBubbleUp = "message to bubble up";
+
+        protected override void When( )
+        {
+            MockFileAdapter.WriteFile( Arg.Any<string>( ), Arg.Any<string>( ) )
+                .Returns( Result.Error( MessageToBubbleUp ) );
+            _result = SUT.WriteTemp( new RenderDataDto( ) );
+        }
+
+        [ Test ]
+        public void Then_Failiure_Is_Returned( )
+        {
+            Assert.AreEqual( MessageToBubbleUp, _result.Msg );
+            Assert.AreEqual( OperationResultEnum.Failed, _result.Status );
+        }
+    }
+}
