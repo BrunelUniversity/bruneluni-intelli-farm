412e539d1be140b20b6c3c2ffa7a73754c0f518e
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-31 19:59:16 +0000

    modified blender data repository, reading data - added failing implementation - tests fail

---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/SceneRepositoryTests/Read/When_Read_Successfully.cs
@@ -0,0 +1,59 @@
+﻿using Aidan.Common.Core;
+using Aidan.Common.Core.Enum;
+using BrunelUni.IntelliFarm.Data.Core.Dtos;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.SceneRepositoryTests.Read
+{
+    public class When_Read_Successfully : Given_A_SceneRepository
+    {
+        private ObjectResult<RenderDataDto> _result;
+
+        protected override void When( )
+        {
+            MockRenderManagerService.RenderManager
+                .GetRenderInfo( )
+                .Returns( new RenderMetaDto
+                {
+                    BlendFilePath = "test"
+                } );
+            MockSceneProcessor.ReadTemp<RenderDataDto>( )
+                .Returns( new ObjectResult<RenderDataDto>
+                {
+                    Status = OperationResultEnum.Success,
+                    Value = new RenderDataDto
+                    {
+                        Samples = 100,
+                        MaxLightBounces = 4
+                    }
+                } );
+            _result = SUT.Read( );
+        }
+
+        [ Test ]
+        public void Then_Success_Is_Returned( )
+        {
+            Assert.AreEqual( OperationResultEnum.Success, _result.Status );
+        }
+
+        [ Test ]
+        public void Then_Blender_Is_Ran_With_Correct_Args( )
+        {
+            MockSceneProcessor
+                .Received( 1 )
+                .RunSceneProcessAndExit( Arg.Any<string>( ), Arg.Any<string>( ), Arg.Any<bool>( ) );
+            MockSceneProcessor
+                .Received( )
+                .RunSceneProcessAndExit( "test", "reader", false );
+        }
+        
+        [ Test ]
+        public void Then_Temp_File_Is_Read_From( )
+        {
+            MockSceneProcessor
+                .Received( 1 )
+                .ReadTemp<RenderDataDto>( );
+        }
+    }
+}
