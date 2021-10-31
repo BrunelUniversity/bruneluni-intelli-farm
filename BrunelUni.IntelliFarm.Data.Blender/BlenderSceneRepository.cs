aa12f56d9b74b687bf4eba24942d47f53eefcaa2
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-31 22:41:26 +0000

    added read scene data - added passing implementation - tests pass

--- BrunelUni.IntelliFarm.Data.Blender/BlenderSceneRepository.cs
+++ BrunelUni.IntelliFarm.Data.Blender/BlenderSceneRepository.cs
@@ -31,7 +31,17 @@ namespace BrunelUni.IntelliFarm.Data.Blender
         }
 
         // TODO: implement
-        public ObjectResult<RenderDataDto> Read( ) => new ObjectResult<RenderDataDto>
-            { Status = OperationResultEnum.Failed, Msg = "implement" };
+        public ObjectResult<RenderDataDto> Read( )
+        {
+            _sceneProcessor.RunSceneProcessAndExit( _renderManagerService.RenderManager.GetRenderInfo( ).BlendFilePath,
+                "reader",
+                false );
+            var data = _sceneProcessor.ReadTemp<RenderDataDto>( );
+            return new ObjectResult<RenderDataDto>
+            {
+                Status = OperationResultEnum.Success,
+                Value = data.Value
+            };
+        }
     }
 }
