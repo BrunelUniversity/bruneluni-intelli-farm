3852acbde8197f92e41ef144524306f75bea0cc4
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-31 23:01:47 +0000

    added read scene data, when file system error occurs - added failing implementation - tests fail

---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/SceneRepositoryTests/Read/When_File_Error_Occurs.cs
@@ -0,0 +1,38 @@
+﻿using Aidan.Common.Core;
+using Aidan.Common.Core.Enum;
+using BrunelUni.IntelliFarm.Data.Core.Dtos;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.SceneRepositoryTests.Read
+{
+    public class When_File_Error_Occurs : Given_A_SceneRepository
+    {
+        private ObjectResult<RenderDataDto> _result;
+        private const string Message = "message";
+        
+        protected override void When( )
+        {
+            MockSceneProcessor.ReadTemp<RenderDataDto>( )
+                .Returns( new ObjectResult<RenderDataDto>
+                {
+                    Status = OperationResultEnum.Failed,
+                    Msg = Message
+                } );
+            MockRenderManagerService
+                .RenderManager
+                .GetRenderInfo( )
+                .Returns( new RenderMetaDto( ) );
+            MockSceneProcessor.RunSceneProcessAndExit( Arg.Any<string>( ), Arg.Any<string>( ), Arg.Any<bool>( ) )
+                .Returns( Result.Success( ) );
+            _result = SUT.Read( );
+        }
+        
+        [ Test ]
+        public void Then_Failiure_Is_Returned( )
+        {
+            Assert.AreEqual( OperationResultEnum.Failed, _result.Status );
+            Assert.AreEqual( Message, _result.Msg );
+        }
+    }
+}
