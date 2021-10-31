2e4461eb16d1ca5a5d07da11c1f9014d990828b3
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-10-31 16:39:06 +0000

    added when blender file is ran with script and rendered - added tests and failing implementation - tests fail

---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/SceneProcessorTests/RunSceneProcessAndExit/When_Scene_Is_Rendered.cs
@@ -0,0 +1,23 @@
+﻿using Aidan.Common.Core;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.SceneProcessorTests.RunSceneProcessAndExit
+{
+    public class When_Scene_Is_Rendered : Given_A_SceneProcessor
+    {
+        protected override void When( )
+        {
+            MockProcessor.RunAndWait( Arg.Any<string>( ), Arg.Any<string>( ) )
+                .Returns( Result.Success( ) );
+            SUT.RunSceneProcessAndExit( "test", "test", true );
+        }
+
+        [ Test ]
+        public void Then_Render_Argument_Is_Passed_Into_Blender_CLI( )
+        {
+            MockProcessor.Received( 1 ).RunAndWait( Arg.Any<string>( ), Arg.Any<string>( ) );
+            MockProcessor.Received( ).RunAndWait( Arg.Any<string>( ), "test -b -P render_test.py -a" );
+        }
+    }
+}
