5d046a3883581e347024a332ee21ec65640cb706
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-11-22 01:30:18 +0000

    added blender environment setup - added failing implementation for when blender cannot be downloaded - tests fail

---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/AnimationContextTests/Initialize/When_File_Cannot_Be_Downloaded.cs
@@ -0,0 +1,35 @@
+﻿using Aidan.Common.Core;
+using Aidan.Common.Core.Enum;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.AnimationContextTests.Initialize
+{
+    public class When_File_Cannot_Be_Downloaded : Given_A_BlenderAnimationContext
+    {
+        private Result _result;
+        private const string ErrorMsg = "error msg";
+
+        protected override void When( )
+        {
+            MockFileAdapter.Exists( Arg.Any<string>( ) )
+                .Returns( Result.Error( "" ) );
+            MockWebClientAdapter.DownloadFile( Arg.Any<string>( ), Arg.Any<string>( ) )
+                .Returns( Result.Error( ErrorMsg ) );
+            _result = SUT.Initialize( );
+        }
+
+        [ Test ]
+        public void Then_Zip_File_Is_Not_Extracted( )
+        {
+            MockZipAdapter.DidNotReceive( ).ExtractToDirectory( Arg.Any<string>( ), Arg.Any<string>( ) );
+        }
+
+        [ Test ]
+        public void Then_Failiure_Is_Returned( )
+        {
+            Assert.AreEqual( OperationResultEnum.Failed, _result.Status );
+            Assert.AreEqual( ErrorMsg, _result.Msg );
+        }
+    }
+}
