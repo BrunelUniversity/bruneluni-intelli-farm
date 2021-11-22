eb6fc29fb48093713d2feeb7fc0f96ed6ec64204
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-11-22 01:34:58 +0000

    added blender environment setup - added failing implementation for when zip cannot be extracted - tests fail

---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/AnimationContextTests/Initialize/When_Zip_File_Cannot_Be_Extracted.cs
@@ -0,0 +1,31 @@
+﻿using Aidan.Common.Core;
+using Aidan.Common.Core.Enum;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.AnimationContextTests.Initialize
+{
+    public class When_Zip_File_Cannot_Be_Extracted : Given_A_BlenderAnimationContext
+    {
+        private Result _result;
+        private const string MessageToBubble = "message to bubble";
+
+        protected override void When( )
+        {
+            MockFileAdapter.Exists( Arg.Any<string>( ) )
+                .Returns( Result.Error( "" ) );
+            MockWebClientAdapter.DownloadFile( Arg.Any<string>( ), Arg.Any<string>( ) )
+                .Returns( Result.Success( ) );
+            MockZipAdapter.ExtractToDirectory( Arg.Any<string>( ), Arg.Any<string>( ) )
+                .Returns( Result.Error( MessageToBubble ) );
+            _result = SUT.Initialize( );
+        }
+
+        [ Test ]
+        public void Then_Failiure_Is_Returned( )
+        {
+            Assert.AreEqual( OperationResultEnum.Failed, _result.Status );
+            Assert.AreEqual( MessageToBubble, _result.Msg );
+        }
+    }
+}
