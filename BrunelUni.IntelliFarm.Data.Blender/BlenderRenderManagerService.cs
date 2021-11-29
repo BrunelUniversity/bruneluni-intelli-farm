02e481b88198b83a1ea885d2947da406d2d8d66f
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-11-29 02:24:43 +0000

    remove inability to set render manager service twice - doesn't work for automated feasibility study

--- BrunelUni.IntelliFarm.Data.Blender/BlenderRenderManagerService.cs
+++ BrunelUni.IntelliFarm.Data.Blender/BlenderRenderManagerService.cs
@@ -4,18 +4,6 @@ namespace BrunelUni.IntelliFarm.Data.Blender
 {
     public class BlenderRenderManagerService : IRenderManagerService
     {
-        private bool _hasBeenSet;
-        private IRenderManager _renderManager;
-        public BlenderRenderManagerService( ) { _hasBeenSet = false; }
-        public IRenderManager RenderManager
-        {
-            get => _renderManager;
-            set
-            {
-                if( _hasBeenSet ) return;
-                _hasBeenSet = true;
-                _renderManager = value;
-            }
-        }
+        public IRenderManager RenderManager { get; set; }
     }
 }
