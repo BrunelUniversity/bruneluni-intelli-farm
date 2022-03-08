7cad2436a4db2b830decaeea704ed3e032874e73
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-03-08 21:37:58 +0000

    added implementation for render scripts in single file

--- BrunelUni.IntelliFarm.Data.Blender/BlenderSceneProcessor.cs
+++ BrunelUni.IntelliFarm.Data.Blender/BlenderSceneProcessor.cs
@@ -8,10 +8,10 @@ namespace BrunelUni.IntelliFarm.Data.Blender
 {
     public class BlenderSceneProcessor : ISceneProcessor
     {
-        private readonly IProcessor _processor;
-        private readonly ISerializer _serializer;
         private readonly IFileAdapter _fileAdapter;
+        private readonly IProcessor _processor;
         private readonly IScriptsRootDirectoryState _scriptsRootDirectoryState;
+        private readonly ISerializer _serializer;
 
         public BlenderSceneProcessor( IProcessor processor,
             ISerializer serializer,
@@ -23,7 +23,7 @@ namespace BrunelUni.IntelliFarm.Data.Blender
             _fileAdapter = fileAdapter;
             _scriptsRootDirectoryState = scriptsRootDirectoryState;
         }
-        
+
         public ObjectResult<T> ReadTemp<T>( ) where T : RenderDto
         {
             var fileResult = _fileAdapter.ReadFile( _scriptsRootDirectoryState.DataScriptsTempFile );
@@ -35,6 +35,7 @@ namespace BrunelUni.IntelliFarm.Data.Blender
                     Msg = fileResult.Msg
                 };
             }
+
             return new ObjectResult<T>
             {
                 Value = _serializer.Deserialize<T>( fileResult.Value ),
@@ -46,16 +47,16 @@ namespace BrunelUni.IntelliFarm.Data.Blender
         {
             var result = _serializer.Serialize( renderDto );
             var fileResult = _fileAdapter.WriteFile( _scriptsRootDirectoryState.DataScriptsTempFile, result );
-            return fileResult.Status == OperationResultEnum.Failed ? fileResult : Result.Success(  );
+            return fileResult.Status == OperationResultEnum.Failed ? fileResult : Result.Success( );
         }
 
         public Result RunSceneProcessAndExit( string pathToBlend, string script, bool render )
         {
-            var args = $"{pathToBlend} -b -P {_scriptsRootDirectoryState.DataScriptsDir}\\render_{script}.py";
+            var args = $"{pathToBlend} -b -P {_scriptsRootDirectoryState.DataScriptsDir}\\render.py";
             if( render )
-            {
                 args += " -a";
-            }
+            else
+                args += $" -- render_{script}";
             var result = _processor.RunAndWait( _scriptsRootDirectoryState.BlenderDirectory, args );
             return result.Status == OperationResultEnum.Failed ? result : Result.Success( );
         }
