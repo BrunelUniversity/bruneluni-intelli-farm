758df62620ae39156dd25b3bd7312a55c7468b2e
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-03-08 23:00:22 +0000

    stubbed out blender ray coverage calculator

---
+++ BrunelUni.IntelliFarm.Data.Blender/BlenderRenderRayCoverageCalculator.cs
@@ -0,0 +1,9 @@
+﻿using BrunelUni.IntelliFarm.Data.Core.Interfaces.Contract;
+
+namespace BrunelUni.IntelliFarm.Data.Blender
+{
+    public class BlenderRenderRayCoverageCalculator : IRenderRayCoverageCalculator
+    {
+        public double GetCoverage( int subdivisions ) { return 0.0; }
+    }
+}
---
+++ BrunelUni.IntelliFarm.Data.Core/Interfaces/Contract/IRenderRayCoverageCalculator.cs
@@ -0,0 +1,7 @@
+﻿namespace BrunelUni.IntelliFarm.Data.Core.Interfaces.Contract
+{
+    public interface IRenderRayCoverageCalculator
+    {
+        double GetCoverage( int subdivisions );
+    }
+}
