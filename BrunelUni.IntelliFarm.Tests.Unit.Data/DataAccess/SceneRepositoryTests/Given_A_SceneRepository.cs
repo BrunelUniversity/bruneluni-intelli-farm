b9ddaba086caee4c38a2c4554612ad344512e2c9
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-03-09 02:30:51 +0000

    satisfied ctor in test fixture

--- BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/SceneRepositoryTests/Given_A_SceneRepository.cs
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/SceneRepositoryTests/Given_A_SceneRepository.cs
@@ -1,4 +1,5 @@
-﻿using Aidan.Common.Utils.Test;
+﻿using Aidan.Common.Core.Interfaces.Contract;
+using Aidan.Common.Utils.Test;
 using BrunelUni.IntelliFarm.Data.Blender;
 using BrunelUni.IntelliFarm.Data.Core.Interfaces.Contract;
 using NSubstitute;
@@ -7,14 +8,16 @@ namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.SceneRepositoryTests
 {
     public class Given_A_SceneRepository : GivenWhenThen<ISceneRepository>
     {
-        protected ISceneProcessor MockSceneProcessor;
+        private ILoggerAdapter<ISceneRepository> _loggerAdapter;
         protected IRenderManagerService MockRenderManagerService;
+        protected ISceneProcessor MockSceneProcessor;
 
         protected override void Given( )
         {
             MockSceneProcessor = Substitute.For<ISceneProcessor>( );
             MockRenderManagerService = Substitute.For<IRenderManagerService>( );
-            SUT = new BlenderSceneRepository( MockSceneProcessor, MockRenderManagerService );
+            _loggerAdapter = Substitute.For<ILoggerAdapter<ISceneRepository>>( );
+            SUT = new BlenderSceneRepository( MockSceneProcessor, MockRenderManagerService, _loggerAdapter );
         }
     }
 }
