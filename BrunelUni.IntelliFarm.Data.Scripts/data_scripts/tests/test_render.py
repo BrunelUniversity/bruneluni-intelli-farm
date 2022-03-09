1c73aca468e74ef69c2414f7093f91f96f07cc0b
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-03-09 17:12:03 +0000

    amended python tests for get coverage - added test that asserts write to temp call

--- BrunelUni.IntelliFarm.Data.Scripts/data_scripts/tests/test_render.py
+++ BrunelUni.IntelliFarm.Data.Scripts/data_scripts/tests/test_render.py
@@ -1,6 +1,6 @@
 from typing import Union
 from unittest import TestCase
-from unittest.mock import Mock, MagicMock
+from unittest.mock import Mock, MagicMock, call
 from mathutils import Vector
 from callee import Captor
 
@@ -18,25 +18,18 @@ class TestRenderCommands(TestCase):
                                     date_time_adapter=self.__date_time_adapter)
 
     def test_when_get_coverage_called(self):
+        self.__comms_service.write_json = MagicMock()
         self.__scene_adapter.cast_ray = MagicMock()
         self.__scene_adapter.cast_ray.side_effect = [True, False, True, True]
         self.__comms_service.read_json = MagicMock(return_value={"subdivisions": 8})
-        self.__scene_adapter.add_iscosphere = MagicMock(return_value=[Vector({0, 0, 1}),
-                                                                      Vector({0, 0, 2}),
-                                                                      Vector({0, 0, 3}),
-                                                                      Vector({0, 0, 4})])
+        self.__scene_adapter.add_iscosphere = MagicMock(return_value=[(0, 0, 1),
+                                                                      (0, 0, 2),
+                                                                      (0, 0, 3),
+                                                                      (0, 0, 4)])
         result = self.__sut.get_scene_and_viewpoint_coverage()
-        self.__scene_adapter.cast_ray.assert_called_with(origin=Vector({0, 0, 0}),
-                                                         direction=Vector({0, 0, 1}),
-                                                         distance=100)
-        self.__scene_adapter.cast_ray.assert_called_with(origin=Vector({0, 0, 0}),
-                                                         direction=Vector({0, 0, 2}),
-                                                         distance=100)
-        self.__scene_adapter.cast_ray.assert_called_with(origin=Vector({0, 0, 0}),
-                                                         direction=Vector({0, 0, 3}),
-                                                         distance=100)
-        self.__scene_adapter.cast_ray.assert_called_with(origin=Vector({0, 0, 0}),
-                                                         direction=Vector({0, 0, 4}),
-                                                         distance=100)
-
-        assert result == 0.75
+        self.__scene_adapter.cast_ray.assert_has_calls([call(origin=(0,0,0), direction=(0,0,1), distance=100),
+                                                       call(origin=(0,0,0), direction=(0,0,2), distance=100),
+                                                       call(origin=(0,0,0), direction=(0,0,3), distance=100),
+                                                       call(origin=(0,0,0), direction=(0,0,4), distance=100)])
+        assert self.__scene_adapter.cast_ray.call_count == 4
+        self.__comms_service.write_json.assert_called_once_with(data={"scene_coverage": 0.75})
