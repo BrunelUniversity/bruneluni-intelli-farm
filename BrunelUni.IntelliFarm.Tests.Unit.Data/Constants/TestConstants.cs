b4ba0c03459c953ca1253a3d8b43fe2f286378ce
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-03-09 18:28:35 +0000

    amended test constants to satisfy build

--- BrunelUni.IntelliFarm.Tests.Unit.Data/Constants/TestConstants.cs
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/Constants/TestConstants.cs
@@ -5,8 +5,11 @@
         public const string Directory = "C:\\Test\\TestUser\\Dir";
         public const string DataScriptsDir = "C:\\Test\\TestUser\\Dir\\data_scripts";
         public const string DataScriptsTempFile = "C:\\Test\\TestUser\\Dir\\data_scripts\\temp\\render.json";
-        public const string BlenderDirectory = "C:\\Test\\TestUser\\Dir\\blender\\blender-2.93.6-windows-x64\\blender.exe";
-        public const string BlenderBaseUrl = "https://download.blender.org/release/Blender2.93";
-        public const string BlenderVersion = "blender-2.93.6-windows-x64";
+
+        public const string BlenderDirectory =
+            "C:\\Test\\TestUser\\Dir\\blender\\blender-2.93.6-windows-x64\\blender.exe";
+
+        public const string BlenderBaseUrl = "https://download.blender.org/release/Blender2.90";
+        public const string BlenderVersion = "blender-2.90.1-windows64";
     }
 }
