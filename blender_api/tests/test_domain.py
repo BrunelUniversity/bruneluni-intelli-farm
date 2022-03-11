8a03a8aa9631f6b03ed849cd8f6c2a68206b96e0
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-03-11 03:10:19 +0000

    added contract for viewport ray casting

--- blender_api/tests/test_domain.py
+++ blender_api/tests/test_domain.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from unittest.mock import Mock, MagicMock, call
 
-from src.core import TempCommsService, SceneAdapter, DateTimeAdapter, MeshEnum
+from src.core import TempCommsService, SceneAdapter, DateTimeAdapter, MeshEnum, OperationEnum
 from src.domain import RenderCommands
 
 
@@ -21,12 +21,15 @@ class TestRenderCommands(TestCase):
         self.__scene_adapter.cast_ray = MagicMock()
         self.__scene_adapter.transform = MagicMock()
         self.__scene_adapter.delete_current_object = MagicMock()
-        self.__scene_adapter.cast_ray.side_effect = [True, False, True, True]
+        self.__scene_adapter.cast_ray.side_effect = [True, False, True, True, True, True, False]
         self.__comms_service.read_json = MagicMock(return_value={"subdivisions": 8})
         self.__scene_adapter.add_mesh = MagicMock(return_value=[(0, 0, 1),
                                                                 (0, 0, 2),
                                                                 (0, 0, 3),
-                                                                (0, 0, 4)])
+                                                                (0, 0, 4),
+                                                                (1, 0, 1),
+                                                                (1, 0, 2),
+                                                                (1, 0, 3)])
 
         # act
         self.__sut.get_scene_and_viewpoint_coverage()
@@ -35,10 +38,25 @@ class TestRenderCommands(TestCase):
         self.__scene_adapter.cast_ray.assert_has_calls([call(origin=(0, 0, 0), direction=(0, 0, 1), distance=100),
                                                         call(origin=(0, 0, 0), direction=(0, 0, 2), distance=100),
                                                         call(origin=(0, 0, 0), direction=(0, 0, 3), distance=100),
-                                                        call(origin=(0, 0, 0), direction=(0, 0, 4), distance=100)])
+                                                        call(origin=(0, 0, 0), direction=(0, 0, 4), distance=100),
+                                                        call(origin=(0, 0, 0), direction=(1, 0, 1), distance=100),
+                                                        call(origin=(0, 0, 0), direction=(1, 0, 2), distance=100),
+                                                        call(origin=(0, 0, 0), direction=(1, 0, 3), distance=100)])
         self.__scene_adapter.add_mesh.assert_called_with(subdivisions=8,
                                                          location=(0, 0, 0),
                                                          mesh=MeshEnum.Iscosphere)
+        self.__scene_adapter.add_mesh.assert_called_with(subdivisions=6,
+                                                         location=(0, 0, 0),
+                                                         mesh=MeshEnum.Plane)
+        self.__scene_adapter.transform.assert_called_with(object="",
+                                                          vector=(-23, 0, 0),
+                                                          operation=OperationEnum.Move)
+        self.__scene_adapter.transform.assert_called_with(object="",
+                                                          vector=(4.698, 8.352, 4.35),
+                                                          operation=OperationEnum.Scale)
+        self.__scene_adapter.transform.assert_called_with(object="",
+                                                          vector=(0, 1.5708, 0),
+                                                          operation=OperationEnum.Rotate)
         self.__scene_adapter.delete_current_object.assert_called_once()
         assert self.__scene_adapter.cast_ray.call_count == 4
-        self.__comms_service.write_json.assert_called_once_with(data={"percentage": 0.75})
+        self.__comms_service.write_json.assert_called_once_with(data={"coverage_percentage": 0.75, "viewport_percentage": float(2/3)})
