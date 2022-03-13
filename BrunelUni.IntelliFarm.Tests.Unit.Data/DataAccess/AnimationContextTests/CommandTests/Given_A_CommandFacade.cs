6f159b1085f872f06762e35184c073975bf2545c
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-03-13 04:12:33 +0000

    add tests for get scene data

---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/AnimationContextTests/CommandTests/Given_A_CommandFacade.cs
@@ -0,0 +1,54 @@
+﻿using Aidan.Common.Utils.Test;
+using BrunelUni.IntelliFarm.Data.Blender;
+using BrunelUni.IntelliFarm.Data.Core.Dtos;
+using BrunelUni.IntelliFarm.Data.Core.Interfaces.Contract;
+using BrunelUni.IntelliFarm.Tests.Unit.Data.Fakes;
+using NSubstitute;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.AnimationContextTests.CommandTests
+{
+    public class Given_A_CommandFacade : GivenWhenThen<ISceneCommandFacade>
+    {
+        private IRenderManagerService _renderManagerService;
+        protected string BlendFile;
+        protected ICommandIn MockCommandIn;
+        protected ICommandInAndOut MockCommandInAndOut;
+        protected ICommandInAndOutFactory MockCommandInAndOutFactory;
+        protected ICommandInFactory MockCommandInFactory;
+        protected ICommandOut MockCommandOut;
+        protected ICommandOutFactory MockCommandOutFactory;
+        protected IScriptsRootDirectoryState ScriptsRootDirectoryState;
+
+        protected override void Given( )
+        {
+            MockCommandOut = Substitute.For<ICommandOut>( );
+            MockCommandIn = Substitute.For<ICommandIn>( );
+            MockCommandInAndOut = Substitute.For<ICommandInAndOut>( );
+            MockCommandInFactory = Substitute.For<ICommandInFactory>( );
+            MockCommandOutFactory = Substitute.For<ICommandOutFactory>( );
+            MockCommandInAndOutFactory = Substitute.For<ICommandInAndOutFactory>( );
+            MockCommandOutFactory
+                .Factory( Arg.Any<CommandMetaDto>( ) )
+                .Returns( MockCommandOut );
+            MockCommandInFactory
+                .Factory( Arg.Any<CommandMetaDto>( ) )
+                .Returns( MockCommandIn );
+            MockCommandInAndOutFactory
+                .Factory( Arg.Any<CommandMetaDto>( ) )
+                .Returns( MockCommandInAndOut );
+            _renderManagerService = Substitute.For<IRenderManagerService>( );
+            BlendFile = "C:\\Users\\blender.blend";
+            _renderManagerService
+                .RenderManager
+                .GetRenderInfo( )
+                .BlendFilePath
+                .Returns( BlendFile );
+            ScriptsRootDirectoryState = new FakeScriptsRootDirectoryState( );
+            SUT = new BlenderSceneCommandFacade( MockCommandInFactory,
+                _renderManagerService,
+                MockCommandOutFactory,
+                MockCommandInAndOutFactory,
+                ScriptsRootDirectoryState );
+        }
+    }
+}
---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/AnimationContextTests/CommandTests/When_Get_Scene_Data_Command_Is_Called.cs
@@ -0,0 +1,36 @@
+﻿using BrunelUni.IntelliFarm.Data.Core.Dtos;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.AnimationContextTests.CommandTests
+{
+    public class When_Get_Scene_Data_Command_Is_Called : Given_A_CommandFacade
+    {
+        private RenderDataDto _dataOut;
+        private RenderDataDto _result;
+
+        protected override void When( )
+        {
+            _dataOut = new RenderDataDto( );
+            MockCommandOut.Run<RenderDataDto>( ).Returns( _dataOut );
+            _result = SUT.GetSceneData( );
+        }
+
+        [ Test ]
+        public void Then_Out_Command_Is_Created( )
+        {
+            MockCommandOutFactory.Received( 1 ).Factory( Arg.Any<CommandMetaDto>( ) );
+            MockCommandOutFactory.Received( ).Factory( Arg.Is<CommandMetaDto>( c =>
+                c.Render == false &&
+                c.Command == "" &&
+                c.RenderMetaDto.BlendFilePath == BlendFile &&
+                c.ScriptsRootDirectoryDto == ScriptsRootDirectoryState.ScriptsRootDirectoryDto ) );
+        }
+
+        [ Test ]
+        public void Then_Out_Command_Is_Ran( ) { MockCommandOut.Received( 1 ).Run<RenderDataDto>( ); }
+
+        [ Test ]
+        public void Then_Correct_Data_Is_Returned( ) { Assert.AreSame( _dataOut, _result ); }
+    }
+}
