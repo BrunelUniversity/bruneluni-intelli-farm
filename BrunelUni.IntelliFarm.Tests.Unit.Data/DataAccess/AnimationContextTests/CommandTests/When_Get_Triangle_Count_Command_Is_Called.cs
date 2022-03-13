57d650d1e32d4604cdc6a131fd84012736ad7009
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-03-13 04:27:37 +0000

    added tests for get triangles and render

---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/AnimationContextTests/CommandTests/When_Get_Triangle_Count_Command_Is_Called.cs
@@ -0,0 +1,34 @@
+﻿using BrunelUni.IntelliFarm.Data.Core.Dtos;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.AnimationContextTests.CommandTests
+{
+    public class When_Get_Triangle_Count_Command_Is_Called : Given_A_CommandFacade
+    {
+        private TriangleCountDto _dataOut;
+        private TriangleCountDto _result;
+
+        protected override void When( )
+        {
+            _dataOut = new TriangleCountDto( );
+            MockCommandOut.Run<TriangleCountDto>( ).Returns( _dataOut );
+            _result = SUT.GetTriangleCount( );
+        }
+
+        [ Test ]
+        public void Then_Out_Command_Is_Created( )
+        {
+            MockCommandOutFactory.Received( 1 ).Factory( Arg.Any<CommandMetaDto>( ) );
+            MockCommandOutFactory
+                .Received( )
+                .Factory( Arg.Is<CommandMetaDto>( c => AssertMeta( c, "get_triangles_count", false ) ) );
+        }
+
+        [ Test ]
+        public void Then_Out_Command_Is_Ran( ) { MockCommandOut.Received( 1 ).Run<TriangleCountDto>( ); }
+
+        [ Test ]
+        public void Then_Correct_Data_Is_Returned( ) { Assert.AreSame( _dataOut, _result ); }
+    }
+}
---
+++ BrunelUni.IntelliFarm.Tests.Unit.Data/DataAccess/AnimationContextTests/CommandTests/When_Render_Command_Is_Called.cs
@@ -0,0 +1,36 @@
+﻿using BrunelUni.IntelliFarm.Data.Core.Dtos;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace BrunelUni.IntelliFarm.Tests.Unit.Data.DataAccess.AnimationContextTests.CommandTests
+{
+    public class When_Render_Command_Is_Called : Given_A_CommandFacade
+    {
+        private RenderResultDto _dataOut;
+        private RenderResultDto _result;
+
+        protected override void When( )
+        {
+            _dataOut = new RenderResultDto( );
+            MockCommandOut.Run<RenderResultDto>( ).Returns( _dataOut );
+            _result = SUT.Render( );
+        }
+
+        [ Test ]
+        public void Then_Out_Command_Is_Created( )
+        {
+            MockCommandOutFactory.Received( 1 ).Factory( Arg.Any<CommandMetaDto>( ) );
+            MockCommandOutFactory
+                .Received( )
+                .Factory( Arg.Is<CommandMetaDto>( c => AssertMeta( c,
+                    "render_frame",
+                    true ) ) );
+        }
+
+        [ Test ]
+        public void Then_Out_Command_Is_Ran( ) { MockCommandOut.Received( 1 ).Run<RenderResultDto>( ); }
+
+        [ Test ]
+        public void Then_Correct_Data_Is_Returned( ) { Assert.AreSame( _dataOut, _result ); }
+    }
+}
