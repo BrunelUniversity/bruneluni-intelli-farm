b2e1c99f23122b924b53c73b2f62dd7d8c56f527
aidangannon <63330150+aidangannon@users.noreply.github.com>
2022-03-14 03:40:25 +0000

    stub out domain project structure and add tests proj

---
+++ BrunelUni.IntelliFarm.Domain/SceneAnalyser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BrunelUni.IntelliFarm.Core.Dtos;
+using BrunelUni.IntelliFarm.Core.Interfaces.Contract;
+
+namespace BrunelUni.IntelliFarm.Domain
+{
+    public class RenderAnalyser : IRenderAnalyser
+    {
+        public(Guid clientId, int [ ] frameNums) [ ] GetFrameNumberBatches(
+            (int framenum, double predictedTime, Guid clientId) frameAnaylsis )
+        {
+            throw new NotImplementedException( );
+        }
+
+        public double GetPredictedTime( ClientDto clientDto, FrameDto frameDto ) { throw new NotImplementedException( ); }
+    }
+}
